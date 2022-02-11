ca576c475a44e2fe675b62ce460237f0b316aeef
HyeokJoon Kong <orijoon98@gmail.com>
2022-02-11 17:14:34 +0900

    Feat: PATCH, PUT, DELETE 메소드 추가

--- http.js
+++ http.js
@@ -17,13 +17,112 @@ exports.post = (test) => {
     try {
       request.post(options, (error, response, responseBody) => {
         if (error != null) {
-          log.error('Request: ' + uri);
+          log.error('Request: ' + uri + ' POST');
           log.error('Body: ' + JSON.stringify(body));
           log.error('Error: ' + error);
           log.error('\n');
           resolve();
         } else {
-          log.info('Request: ' + uri);
+          log.info('Request: ' + uri + ' POST');
+          log.info('Body: ' + JSON.stringify(body));
+          log.info('Response: ' + JSON.stringify(responseBody));
+          log.info('\n');
+          resolve();
+        }
+      });
+    } catch (e) {
+      reject();
+    }
+  });
+};
+
+exports.patch = (test) => {
+  let uri = test['uri'];
+  let body = test['body'];
+  const options = {
+    uri: uri,
+    method: 'PATCH',
+    body: body,
+    json: true,
+  };
+
+  return new Promise((resolve, reject) => {
+    try {
+      request.patch(options, (error, response, responseBody) => {
+        if (error != null) {
+          log.error('Request: ' + uri + ' PATCH');
+          log.error('Body: ' + JSON.stringify(body));
+          log.error('Error: ' + error);
+          log.error('\n');
+          resolve();
+        } else {
+          log.info('Request: ' + uri + ' PATCH');
+          log.info('Body: ' + JSON.stringify(body));
+          log.info('Response: ' + JSON.stringify(responseBody));
+          log.info('\n');
+          resolve();
+        }
+      });
+    } catch (e) {
+      reject();
+    }
+  });
+};
+
+exports.put = (test) => {
+  let uri = test['uri'];
+  let body = test['body'];
+  const options = {
+    uri: uri,
+    method: 'PUT',
+    body: body,
+    json: true,
+  };
+
+  return new Promise((resolve, reject) => {
+    try {
+      request.put(options, (error, response, responseBody) => {
+        if (error != null) {
+          log.error('Request: ' + uri + ' PUT');
+          log.error('Body: ' + JSON.stringify(body));
+          log.error('Error: ' + error);
+          log.error('\n');
+          resolve();
+        } else {
+          log.info('Request: ' + uri + ' PUT');
+          log.info('Body: ' + JSON.stringify(body));
+          log.info('Response: ' + JSON.stringify(responseBody));
+          log.info('\n');
+          resolve();
+        }
+      });
+    } catch (e) {
+      reject();
+    }
+  });
+};
+
+exports.delete = (test) => {
+  let uri = test['uri'];
+  let body = test['body'];
+  const options = {
+    uri: uri,
+    method: 'DELETE',
+    body: body,
+    json: true,
+  };
+
+  return new Promise((resolve, reject) => {
+    try {
+      request.delete(options, (error, response, responseBody) => {
+        if (error != null) {
+          log.error('Request: ' + uri + ' DELETE');
+          log.error('Body: ' + JSON.stringify(body));
+          log.error('Error: ' + error);
+          log.error('\n');
+          resolve();
+        } else {
+          log.info('Request: ' + uri + ' DELETE');
           log.info('Body: ' + JSON.stringify(body));
           log.info('Response: ' + JSON.stringify(responseBody));
           log.info('\n');
--- test.js
+++ test.js
@@ -15,13 +15,43 @@ exports.run = async () => {
       for (let i = 0; i < tests.length; i++) {
         tests[i] = env.variable(tests[i]);
         tests[i] = env.random(tests[i]);
-        await http.post(tests[i]);
+        switch (tests[i]['method']) {
+          case 'POST':
+            await http.post(tests[i]);
+            break;
+          case 'PATCH':
+            await http.patch(tests[i]);
+            break;
+          case 'PUT':
+            await http.put(tests[i]);
+            break;
+          case 'DELETE':
+            await http.delete(tests[i]);
+            break;
+          default:
+            break;
+        }
       }
     } else {
       // 직접 등록한 테스트인 경우
       test = env.variable(test);
       test = env.random(test);
-      await http.post(test);
+      switch (test['method']) {
+        case 'POST':
+          await http.post(test);
+          break;
+        case 'PATCH':
+          await http.patch(test);
+          break;
+        case 'PUT':
+          await http.put(test);
+          break;
+        case 'DELETE':
+          await http.delete(test);
+          break;
+        default:
+          break;
+      }
     }
   }
 };
